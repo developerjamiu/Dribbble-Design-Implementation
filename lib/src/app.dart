6740b607430a5b65a116dda482db562d240cc954
Okanlawon Jamiu <mistaglobe@gmail.com>
2020-10-30 17:09:52 +0100

    Initial Project Structure

---
+++ lib/src/app.dart
@@ -0,0 +1,16 @@
+import 'package:dribbble_design/src/views/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/src/views/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AnnotatedRegion(
+      value: SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+      child: Scaffold(),
+    );
+  }
+}
